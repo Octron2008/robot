12081717c18be09658ef95c7c5197b17e94621d4
Octron2008 <129083398+Octron2008@users.noreply.github.com>
2023-03-30 17:51:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ dist=0
 rawdist=0
 pins.digital_write_pin(DigitalPin.P15,1)
 basic.pause(500)
-set_dist=15
+set_dist=10
+time1=0
 
 while(True):
     pins.digital_write_pin(DigitalPin.P15,0)
@@ -10,9 +11,9 @@ while(True):
     basic.pause(200)
     rawdist=pins.analog_read_pin(AnalogPin.P2)
     dist=rawdist*1.1
-    basic.show_number(dist)
     basic.pause(500)
-    if dist < set_dist:
-        input.running_time()
-        kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR1)
-        kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR2)
+    for i in range(4):
+        if dist > set_dist:
+            input.running_time()
+            kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1, kitronik_motor_driver.MotorDirection.FORWARD, 20)
+            kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2, kitronik_motor_driver.MotorDirection.FORWARD, 20)
