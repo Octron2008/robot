5a31cc10105e1b0724978d3d22a81d55c61966e8
Octron2008 <129083398+Octron2008@users.noreply.github.com>
2023-03-30 17:43:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-def on_button_pressed_a():
-    kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR1,
-        kitronik_motor_driver.MotorDirection.FORWARD,
-        97)
-    kitronik_motor_driver.motor_on(kitronik_motor_driver.Motors.MOTOR2,
-        kitronik_motor_driver.MotorDirection.FORWARD,
-        100)
-    basic.pause(1000)
-    kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR1)
-    kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR2)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+dist=0
+rawdist=0
+pins.digital_write_pin(DigitalPin.P15,1)
+basic.pause(500)
+set_dist=15
+
+while(True):
+    pins.digital_write_pin(DigitalPin.P15,0)
+    pins.digital_write_pin(DigitalPin.P15,1)
+    basic.pause(200)
+    rawdist=pins.analog_read_pin(AnalogPin.P2)
+    dist=rawdist*1.1
+    basic.show_number(dist)
+    basic.pause(500)
+    if dist < set_dist:
+        input.running_time()
+        kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR1)
+        kitronik_motor_driver.motor_off(kitronik_motor_driver.Motors.MOTOR2)
